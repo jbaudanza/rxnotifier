bdb78940988a6623892a2c28c77d3b96cbe6b30e
Jonathan Baudanza <jon@jonb.org>
2017-01-01 14:47:50 -0800

    add MemoryNotifier to index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('babel-polyfill')
 module.exports = {
   PgNotifier: require('./lib/pg_notifier').default,
-  RedisNotifier: require('./lib/redis_notifier').default
+  RedisNotifier: require('./lib/redis_notifier').default,
+  MemoryNotifier: require('./lib/memory_notifier').default
 };
